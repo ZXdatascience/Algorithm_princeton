5dadca17849833bfb350f79b9d889d1e8692488f
ZXdatascience <xhustzhang@163.com>
2017-06-16 22:44:40 -0400

    Add files via upload

--- Assignment1_percolation/PercolationStats.java
+++ Assignment1_percolation/PercolationStats.java
@@ -1,36 +1,34 @@
 import edu.princeton.cs.algs4.*;
 
 public class PercolationStats{
-	double[] result;
-	int trials;
+	final private double[] result;
+	final private int trials;
 	public PercolationStats(int n, int trials){
-		this.trials= trials;
+		this.trials = trials;
 		result= new double[trials];
 		experiments(n);
 	}
 	
 	private void experiments(int n){
-		int numberOfTrial= 0;
-		while(numberOfTrial< trials){
-			Percolation percolation= new Percolation(n);
-			double num= 0;
+		int numberOfTrial = 0;
+		while(numberOfTrial < trials){
+			Percolation percolation = new Percolation(n);
+			double num = 0;
 			int[] rowColNum;
-			rowColNum= StdRandom.permutation(n*n);
-			for(int i=0; i<n*n;i++){
+			rowColNum = StdRandom.permutation(n*n);
+			for(int i =0; i<n*n; i++){
 					percolation.open(rowColNum[i]/n+1, rowColNum[i]%n+1);
-					System.out.println("coordinate: "+ (rowColNum[i]/n+1)+", "+ (rowColNum[i]%n+1));
 					num++;
 					if(percolation.percolates()) break;
 					}
-			System.out.println(num);
-			result[numberOfTrial]= num/(n*n);
+			result[numberOfTrial] = num/(n*n);
 			numberOfTrial++;
 			}
 	}
 	
 	
 	public double mean(){
-		double sum=0;
+		double sum = 0;
 	    for (int i = 0; i < result.length; i++) {
 	        sum += result[i];
 	    }
@@ -41,7 +39,7 @@ public class PercolationStats{
 		double sd = 0;
 		for (int i = 0; i < result.length; i++)
 		{
-			double average= mean();
+			double average = mean();
 		    sd += (result[i] - average)*(result[i] - average) / (result.length-1);
 		}
 		double standardDeviation = Math.sqrt(sd);
@@ -58,15 +56,15 @@ public class PercolationStats{
 	
 	public static void main(String[] args){
 		int input[] = new int[2];
-		for (int i=0;i<2;i++){
-			String item= StdIn.readString();
-			input[i]= Integer.parseInt(item);
+		for (int i=0; i<2; i++){
+			String item = StdIn.readString();
+			input[i] = Integer.parseInt(item);
 		}
 		PercolationStats percolationStats= new PercolationStats(input[0], input[1]);
 		
-		StdOut.println("mean			= "+ percolationStats.mean());
-		StdOut.println("stddev			= "+ percolationStats.stddev());
-		StdOut.println("95% confidence interval	= "+ "["+percolationStats.confidenceLo()+", "+ percolationStats.confidenceHi()+ "]");
+		StdOut.println("mean			= " + percolationStats.mean());
+		StdOut.println("stddev			= " + percolationStats.stddev());
+		StdOut.println("95% confidence interval	= " + "["+percolationStats.confidenceLo() +", " + percolationStats.confidenceHi() + "]");
 	}
 	
 }

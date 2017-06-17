f2a3f9c08f068b673bd1033dd83201fa1dc46680
ZXdatascience <xhustzhang@163.com>
2017-06-17 18:32:46 -0400

    Add files via upload

--- Assignment1_percolation/Percolation.java
+++ Assignment1_percolation/Percolation.java
@@ -1,12 +1,12 @@
-import edu.princeton.cs.algs4.StdOut;
 import java.util.Arrays;
-import java.lang.*;
+import edu.princeton.cs.algs4.*;
+
 public class Percolation {
 	private Site[][] grid;
 	private Site topVirtualSite;
 	private Site bottomVirtualSite;
-        private int sizeSite;
 	private int openNumber = 0;
+	private int siteSize;
 	private int[][] size;
 	private class Site {
 		private int row;
@@ -19,73 +19,49 @@ public class Percolation {
 			this.isOpen = false;
 		}
 		private boolean equals(Site other){
-			return (this.row == other.row)&& (this.col == other.col);
+			return (this.row == other.row)&& (this.col== other.col);
 		}
 	}
 	public Percolation(int n){
 		// create n-by-n grid, with all sites blocked
-                if (n <= 0){
-                    throw new IndexOutOfBoundsException();
-                }
-		grid = new Site[n][n];
-		size = new int[n][n];
-                sizeSite = n;
+		if (n <= 0) throw new java.lang.IllegalArgumentException();
+		grid = new Site[n+2][n+2];
+		size = new int[n+2][n+2];
+		siteSize = n;
 		for (int[] row: size)
 		    Arrays.fill(row, 1);
-		for(int i=0; i<n; i++){
-			for(int j=0; j<n; j++){
-				grid[i][j] = new Site(i+1,j+1);
-				grid[i][j].parent = grid[i][j];
+		for(int i=0; i<n+2; i++){
+			for(int j=0; j<n+2; j++){
+				grid[i][j] = new Site(i,j);
+				grid[i][j].parent= grid[i][j];
 			}
 		}
-		topVirtualSite = new Site(0, 0);
-                topVirtualSite.parent = topVirtualSite;
-		bottomVirtualSite = new Site(sizeSite + 1 , sizeSite + 1);
-                bottomVirtualSite.parent = bottomVirtualSite;
+		topVirtualSite = grid[0][0];
+		bottomVirtualSite = grid[n+1][n+1];
 	}
-	private void checkIndex(int row, int col){
-            if (row <= 0 || col <= 0 || row > sizeSite || col >  sizeSite){
-                throw new IndexOutOfBoundsException();
-            }
-        }
+	
+	private void checkBound(int row, int col){
+		if (row <= 0 || row > siteSize || col <= 0 || col > siteSize) throw new IndexOutOfBoundsException();
+	}
 	public void open(int row, int col){
-                checkIndex(row, col);
-                row--; col--;
-                if(row ==0) union(grid[row][col], topVirtualSite);
-                else if(row == sizeSite) union(grid[row][col], bottomVirtualSite);
+		checkBound(row, col);
+		if (row == 1) union(grid[row][col], topVirtualSite);
+		if (row == siteSize) union(grid[row][col], bottomVirtualSite);
 		grid[row][col].isOpen = true;
 		openNumber++;
-                try{
-                    if(grid[row+1][col].isOpen) union(grid[row][col], grid[row+1][col]);}
-                catch(IndexOutOfBoundsException e){
-                    StdOut.println("outofbounds");}
-                
-                try{
-                    if(grid[row-1][col].isOpen) union(grid[row][col], grid[row-1][col]);}
-                catch(IndexOutOfBoundsException e){
-                    StdOut.println("outofbounds");}
-                
-                try{
-                    if(grid[row][col+1].isOpen) union(grid[row][col], grid[row][col+1]);}
-                catch(IndexOutOfBoundsException e){
-                    StdOut.println("outofbounds");}
-                
-                try{
-                    if(grid[row][col-1].isOpen) union(grid[row][col], grid[row][col-1]);}
-                catch(IndexOutOfBoundsException e){
-                    StdOut.println("outofbounds");}
+		if(grid[row+1][col].isOpen) union(grid[row][col], grid[row+1][col]);
+		if(grid[row-1][col].isOpen) union(grid[row][col], grid[row-1][col]);
+		if(grid[row][col+1].isOpen) union(grid[row][col], grid[row][col+1]);
+		if(grid[row][col-1].isOpen) union(grid[row][col], grid[row][col-1]);
 	}
-                
 	
 	public boolean isOpen(int row, int col){
-                checkIndex(row, col);
-                row--; col--;
+		checkBound(row, col);
 		return grid[row][col].isOpen == true;
 	}
 	
 	public boolean isFull(int row, int col){
-                checkIndex(row, col);
-                row--; col--;
+		checkBound(row, col);
 		return connected(grid[row][col], topVirtualSite);
 	}
 	
@@ -98,8 +74,8 @@ public class Percolation {
 	}
 	
 	private void union(Site p, Site q){
-		 Site rootp= root(p);
-		 Site rootq= root(q);
+		 Site rootp = root(p);
+		 Site rootq = root(q);
 		 if (size[rootp.row][rootp.col] >= size[rootq.row][rootq.col]){
 			 rootq.parent = rootp;
 			 size[rootp.row][rootp.col] += size[rootq.row][rootq.col];
@@ -121,20 +97,22 @@ public class Percolation {
 		return root(p).equals(root(q));
 	}
 	public static void main(String[] args){
-		Percolation percolation = new Percolation(4);
+		Percolation percolation= new Percolation(5);
 		percolation.open(1, 1);
 		percolation.open(1, 2);
-		percolation.open(2, 2);
+		percolation.open(1, 3);
 		percolation.open(2, 3);
-		percolation.open(2, 4);
+		StdOut.println(percolation.openNumber);
+		percolation.open(3, 3);
+		percolation.open(4, 3);
+		percolation.open(2, 2);
+		StdOut.println(percolation.openNumber);
+		if (percolation.isFull(5, 3)) StdOut.println("4, 3 in full");
 		if (percolation.percolates()){
-		    System.out.println("The square is perculated");
+			System.out.println("The square is perculated");
 		}
 		else {
-                    System.out.println("The square is not perculated");
+			System.out.println("The square is not perculated");
 		}
-                if (percolation.isFull(2, 4)){
-                    System.out.println("3,5 is Full");
-                }
 	}
 }
--- Assignment1_percolation/PercolationStats.java
+++ Assignment1_percolation/PercolationStats.java
@@ -1,26 +1,28 @@
 import edu.princeton.cs.algs4.*;
 
 public class PercolationStats{
-	final private double[] result;
-	final private int trials;
+	private double[] result;
+	private int trials;
 	public PercolationStats(int n, int trials){
 		this.trials = trials;
-		result= new double[trials];
+		result = new double[trials];
 		experiments(n);
 	}
 	
 	private void experiments(int n){
 		int numberOfTrial = 0;
 		while(numberOfTrial < trials){
-			Percolation percolation = new Percolation(n);
+			Percolation percolation= new Percolation(n);
 			double num = 0;
 			int[] rowColNum;
-			rowColNum = StdRandom.permutation(n*n);
-			for(int i =0; i<n*n; i++){
-					percolation.open(rowColNum[i]/n+1, rowColNum[i]%n+1);
-					num++;
+			rowColNum= StdRandom.permutation(n*n);
+			for(int i=0; i<n*n;i++){
 					if(percolation.percolates()) break;
+					num++;
+					percolation.open(rowColNum[i]/n+1, rowColNum[i]%n+1);
+					StdOut.println(percolation.numberOfOpenSites());
 					}
+			StdOut.println("percolate number" +num);
 			result[numberOfTrial] = num/(n*n);
 			numberOfTrial++;
 			}
@@ -47,24 +49,23 @@ public class PercolationStats{
 	}
 	
 	public double confidenceLo(){
-		return mean() - stddev()*1.96/Math.sqrt(result.length);
+		return mean() - stddev() * 1.96 / Math.sqrt(result.length);
 	}
 	
 	public double confidenceHi(){
-		return mean() + stddev()*1.96/Math.sqrt(result.length);
+		return mean() + stddev() * 1.96 / Math.sqrt(result.length);
 	}
 	
 	public static void main(String[] args){
 		int input[] = new int[2];
-		for (int i=0; i<2; i++){
-			String item = StdIn.readString();
-			input[i] = Integer.parseInt(item);
+		for (int i=0;i<2;i++){
+			String item= StdIn.readString();
+			input[i]= Integer.parseInt(item);
 		}
 		PercolationStats percolationStats= new PercolationStats(input[0], input[1]);
-		
-		StdOut.println("mean			= " + percolationStats.mean());
-		StdOut.println("stddev			= " + percolationStats.stddev());
-		StdOut.println("95% confidence interval	= " + "["+percolationStats.confidenceLo() +", " + percolationStats.confidenceHi() + "]");
+		StdOut.println("mean			= "+ percolationStats.mean());
+		StdOut.println("stddev			= "+ percolationStats.stddev());
+		StdOut.println("95% confidence interval	= "+ "["+percolationStats.confidenceLo()+", "+ percolationStats.confidenceHi()+ "]");
 	}
 	
 }
